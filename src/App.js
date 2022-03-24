648190847bff55102938f93ad7a6145fbde931b5
Tanay <tanay.joshi9@gmail.com>
2022-03-24 18:34:06 +0530

    chore: removed unwanted code

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ function App() {
         <Route path="/wishlist" element={<Wishlist />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/products" element={<Products/>}></Route>
-        <Route path="/products/fiction" element={<Products category='Fiction'/>}></Route>
-        <Route path="/products/nonfiction" element={<Products category='Non-Fiction'/>}></Route>
-        <Route path="/products/selfhelp" element={<Products category='Self-Help'/>}></Route>
-        <Route path="/products/boxset" element={<Products category='Box-set'/>}></Route>
-        <Route path="/products/justin" element={<Products category='Just In'/>}></Route>
-        <Route path="/products/popular" element={<Products category='Popular'/>}></Route>
+        <Route path="/products" element={<Products />}></Route>
       </Routes>
     </>
   );
