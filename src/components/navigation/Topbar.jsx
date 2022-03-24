59a060bcf0d1215b73bbe90e4ca824728ba14292
Tanay <tanay.joshi9@gmail.com>
2022-03-24 18:31:32 +0530

    refactor,style: link styling and link path

--- src/components/navigation/Topbar.jsx
+++ src/components/navigation/Topbar.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Topbar = () => {
   return (
     <nav className={`${styles.topbar_container}`}>
-      <Link to="/" className="active">
+      <Link to="/" className="link link-dark">
         Home
       </Link>
-      <Link to="" className="link link-dark" href="/pages/products.html">
+      <Link to="/products" className="link link-dark">
         All Products
       </Link>
       <Link to="" className="link link-dark">
