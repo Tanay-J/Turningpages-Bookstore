a3182e2f6318fe5f09a70d421b8adfca3bf67fc1
Tanay <tanay.joshi9@gmail.com>
2022-03-28 20:22:44 +0530

    feat: use cart and wishlist context providers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import { makeServer } from "./server";
 import { FilterProvider } from "./contexts/filter-context";
 import "./index.css";
+import { CartProvider } from "./contexts/cart-context";
+import { WishlistProvider } from "./contexts/wishlist-context";
 
 // Call make Server
 makeServer();
@@ -12,9 +14,13 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <FilterProvider>
-        <App />
-      </FilterProvider>
+      <CartProvider>
+        <WishlistProvider>
+          <FilterProvider>
+            <App />
+          </FilterProvider>
+        </WishlistProvider>
+      </CartProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
