ea9f2f454dc2f37d9776ffe8b911e376a02b1d42
Tanay <tanay.joshi9@gmail.com>
2022-03-28 20:19:27 +0530

    feat: function to get billing details of cart items

---
+++ src/utils/getBillingDetails.jsx
@@ -0,0 +1,39 @@
+import { useCart } from "../contexts/cart-context";
+
+const getBillingDetails = () => {
+  let delivery = 0;
+  const { cartState } = useCart();
+  const { cartItems } = cartState;
+
+  const totalAmount = cartItems.reduce(
+    (acc, curr) => Number(curr.price) * Number(curr.qty) + acc,
+    0
+  );
+
+  const totalQty = cartItems.reduce((acc, curr) => Number(curr.qty) + acc, 0);
+
+  const amountAfterDiscount = cartItems.reduce(
+    (acc, curr) =>
+      Math.floor(
+        Number(curr.price) - (Number(curr.price) * Number(curr.discount)) / 100
+      ) *
+        Number(curr.qty) +
+      acc,
+    0
+  );
+  const totalDiscount = amountAfterDiscount - totalAmount;
+
+  cartItems.length > 0 && amountAfterDiscount < 499
+    ? (delivery = 40)
+    : (delivery = 0);
+
+  return {
+    totalAmount,
+    totalQty,
+    amountAfterDiscount,
+    totalDiscount,
+    delivery,
+  };
+};
+
+export { getBillingDetails };
