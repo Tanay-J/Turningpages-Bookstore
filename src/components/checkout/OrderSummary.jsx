1ac89818470ad460603322baf76fdd2bfae5875e
Tanay <tanay.joshi9@gmail.com>
2022-06-16 15:30:13 +0530

    fix: navigation after order placement completion

--- src/components/checkout/OrderSummary.jsx
+++ src/components/checkout/OrderSummary.jsx
@@ -65,53 +65,72 @@ const OrderSummary = () => {
   };
 
   return (
-    <section className="mx-auto">
-      <p className="text-xs font-bold">Order Summary</p>
-      <div className={`${styles.summary} text-center my-m`}>
-        <p className="text-left font-bold">Book</p>
-        <p className=" font-bold">Quantity</p>
-        <p className=" font-bold">Price</p>
-        <p className=" font-bold">Amount</p>
-        {cartItems.map((item) => (
-          <Fragment key={item._id}>
-            <p className="text-left">
-              {item.title} <span className="text-gray">by {item.author}</span>
+    <>
+      {cartItems.length ? (
+        <section className="mx-auto">
+          <p className="text-xs font-bold">Order Summary</p>
+          <div className={`${styles.summary} text-center my-m`}>
+            <p className="text-left font-bold">Book</p>
+            <p className=" font-bold">Quantity</p>
+            <p className=" font-bold">Price</p>
+            <p className=" font-bold">Amount</p>
+            {cartItems.map((item) => (
+              <Fragment key={item._id}>
+                <p className="text-left">
+                  {item.title}{" "}
+                  <span className="text-gray">by {item.author}</span>
+                </p>
+                <p>{item.qty}</p>
+                <p>₹{item.price}</p>
+                <p>₹{item.price * item.qty}</p>
+              </Fragment>
+            ))}
+
+            <hr className="width-100"></hr>
+            <hr className="width-100"></hr>
+            <hr className="width-100"></hr>
+            <hr className="width-100"></hr>
+
+            <p className="text-left">Total</p>
+            <p>{totalQty}</p>
+            <p></p>
+            <p>₹{totalAmount}</p>
+
+            <p className="text-left">Discount</p>
+            <p></p>
+            <p></p>
+            <p className="text-success  font-bold">
+              -₹{Math.abs(totalDiscount)}
             </p>
-            <p>{item.qty}</p>
-            <p>₹{item.price}</p>
-            <p>₹{item.price * item.qty}</p>
-          </Fragment>
-        ))}
 
-        <hr className="width-100"></hr>
-        <hr className="width-100"></hr>
-        <hr className="width-100"></hr>
-        <hr className="width-100"></hr>
+            <p className="text-left">Delivery</p>
+            <p>{}</p>
+            <p></p>
+            <p>₹{delivery}</p>
 
-        <p className="text-left">Total</p>
-        <p>{totalQty}</p>
-        <p></p>
-        <p>₹{totalAmount}</p>
-
-        <p className="text-left">Discount</p>
-        <p></p>
-        <p></p>
-        <p className="text-success  font-bold">-₹{Math.abs(totalDiscount)}</p>
-
-        <p className="text-left">Delivery</p>
-        <p>{}</p>
-        <p></p>
-        <p>₹{delivery}</p>
-
-        <p className="text-left text-xs font-bold">Final Amount</p>
-        <p className=" text-xs font-bold"></p>
-        <p></p>
-        <p className=" text-xs font-bold">₹{amountAfterDiscount + delivery}</p>
-      </div>
-      <button className="btn btn-primary width-100" onClick={paymentHandler}>
-        PLACE ORDER
-      </button>
-    </section>
+            <p className="text-left text-xs font-bold">Final Amount</p>
+            <p className=" text-xs font-bold"></p>
+            <p></p>
+            <p className=" text-xs font-bold">
+              ₹{amountAfterDiscount + delivery}
+            </p>
+          </div>
+          <button
+            className="btn btn-primary width-100"
+            onClick={paymentHandler}
+          >
+            PLACE ORDER
+          </button>
+        </section>
+      ) : (
+        <section className=" mx-auto text-center">
+          <p className="my-xs text-xs">Your Cart is empty</p>
+          <p className="my-xs text-xs">
+            To Checkout and Place Order, Please add Products to the Cart
+          </p>{" "}
+        </section>
+      )}
+    </>
   );
 };
 export { OrderSummary };
